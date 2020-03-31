739b27e3c986f4781ce86844812d8734f21ef400
JP Lemieux <phillemieux66@hotmail.com>
2020-03-31 12:38:47 -0400

    Support Lazy Loading

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,57 +1,89 @@
+import { graphql } from "gatsby"
 import React from "react"
+import InfiniteScroll from "react-infinite-scroller"
 
-import indexStyles from "./index.module.scss"
 import Layout from "../components/layout"
 import RestaurantCard from "../components/restaurant-card"
-import { useStaticQuery, graphql } from "gatsby"
+import indexStyles from "./index.module.scss"
 
-const IndexPage = () => {
-  const data = useStaticQuery(graphql`
-    query {
-      allAirtable {
-        nodes {
-          data {
-            Address
-            Name
-            Gift_Card_Link
-            Image {
-              localFiles {
-                relativePath
-              }
+class IndexPage extends React.Component {
+  constructor() {
+    super()
+    this.addCards = this.addCards.bind(this)
+
+    this.state = {
+      cardsToShow: 12,
+    }
+  }
+
+  addCards() {
+    this.setState({ cardsToShow: this.state.cardsToShow + 12 })
+  }
+
+  render() {
+    const { data } = this.props
+    const { cardsToShow } = this.state
+
+    const nodes = data.allAirtable.nodes.slice(0, cardsToShow)
+
+    return (
+      <Layout>
+        <div className={indexStyles.description}>
+          <h1>
+            Tes restaurants préférés à Montréal risquent de fermer. Contribue à
+            les sauver.
+          </h1>
+          <p>
+            Les cartes cadeaux permettent "d'applatir la courbe" des revenus
+            perdus du COVID-19.
+          </p>
+        </div>
+
+        <InfiniteScroll
+          pageStart={0}
+          loadMore={this.addCards}
+          hasMore={cardsToShow < data.allAirtable.nodes.length}
+          loader={
+            <div className="loader" key={0}>
+              Loading ...
+            </div>
+          }
+        >
+          <div className={indexStyles.restaurantsList}>
+            {nodes.map(node => {
+              return (
+                <RestaurantCard
+                  key={node.data.Name}
+                  name={node.data.Name}
+                  link={node.data.Gift_Card_Link}
+                  image={node.data.Image.localFiles[0].relativePath}
+                ></RestaurantCard>
+              )
+            })}
+          </div>
+        </InfiniteScroll>
+      </Layout>
+    )
+  }
+}
+
+export const pageQuery = graphql`
+  query {
+    allAirtable {
+      nodes {
+        data {
+          Address
+          Name
+          Gift_Card_Link
+          Image {
+            localFiles {
+              relativePath
             }
           }
         }
       }
     }
-  `)
-
-  return (
-    <Layout>
-      <div className={indexStyles.description}>
-        <h1>
-          Tes restaurants préférés à Montréal risquent de fermer. Contribue à
-          les sauver.
-        </h1>
-        <p>
-          Les cartes cadeaux permettent "d'applatir la courbe" des revenus
-          perdus du COVID-19.
-        </p>
-      </div>
-
-      <div className={indexStyles.restaurantsList}>
-        {data.allAirtable.nodes.map(node => {
-          return (
-            <RestaurantCard
-              key={node.data.Name}
-              name={node.data.Name}
-              link={node.data.Gift_Card_Link}
-              image={node.data.Image.localFiles[0].relativePath}
-            ></RestaurantCard>
-          )
-        })}
-      </div>
-    </Layout>
-  )
-}
+  }
+`
 
 export default IndexPage
