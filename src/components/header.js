afa864f49548550212e5847f557d9617b4e19ee0
JP Lemieux <phillemieux66@hotmail.com>
2020-04-04 11:57:49 -0400

    Add Header

--- src/components/header.js
+++ src/components/header.js
@@ -3,7 +3,18 @@ import React from "react"
 import headerStyles from "./header.module.scss"
 
 const Header = () => {
-  return <header className={headerStyles.header}></header>
+  return (
+    <header className={headerStyles.header}>
+      <div></div>
+
+      <div className={headerStyles.headerButtonsContainer}>
+        <a>FAQ</a>
+        <a href="https://airtable.com/shrDHAlRbrk4lpgZn" target="_blank">
+          Ajouter un restaurant
+        </a>
+      </div>
+    </header>
+  )
 }
 
 export default Header
